a35fd0e0e08938ea070b40bf5ea891bf8944caf7
JoseMiguelPomares <josedeberes2709@gmail.com>
2025-03-17 11:21:36 +0100

    Primer push apirest

---
+++ SwapifyAPI/src/main/kotlin/com/example/swapifyapi/SwapifyApiApplication.kt
@@ -0,0 +1,11 @@
+package com.example.swapifyapi
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SwapifyApiApplication
+
+fun main(args: Array<String>) {
+    runApplication<SwapifyApiApplication>(*args)
+}
