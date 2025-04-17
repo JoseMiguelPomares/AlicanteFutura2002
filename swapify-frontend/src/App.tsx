c89286105a26a5195101194b68a4a63be89f4605
JoseMiguelPomares <josedeberes2709@gmail.com>
2025-04-17 12:48:18 +0200

    Categoria

--- swapify-frontend/src/App.tsx
+++ swapify-frontend/src/App.tsx
@@ -11,6 +11,7 @@ import { PaginaVender } from "./pages/PaginaVender"
 import { ScrollToTop } from "./components/ScrollToTop"
 import { PaginaBusqueda } from "./pages/PaginaBusqueda"
 import { AuthProvider } from "./contexts/AuthContext"
+import { PaginaPorCategoria } from "./pages/PaginaPorCategoria"
 
 // Componente principal de la aplicación
 export const App: React.FC = () => {
