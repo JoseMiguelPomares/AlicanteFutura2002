cf0ee50c2cd5deddaab30fc7d1a6b8feef817995
JoseMiguelPomares <josedeberes2709@gmail.com>
2025-04-25 11:25:10 +0200

    Importe de dependencias

---
+++ swapify-frontend/src/components/Mapa.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { View, StyleSheet, Text, Image } from "react-native";
+import MapView, { Marker } from "react-native-maps";
+
+export default function Mapa() {
+    return(
+        <View>
+            <MapView></MapView>
+        </View>
+    );
+}
